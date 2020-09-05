57d86e87f62b08398919b305b23185a09e1be7a3
José Eugênio dos Santos <37482325+J-Eugenio@users.noreply.github.com>
2020-09-05 15:36:22 -0300

    Estilo do Dashboard

--- github-explorer/src/pages/Dashboard/index.tsx
+++ github-explorer/src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react'
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Titulo } from './styles'
+import logoImg from '../../assets/logo.svg'
+
+import { Titulo, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
-    return <Titulo>Explore repositórios no Github</Titulo>
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Titulo>Explore repositórios no Github</Titulo>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img
+                        src="https://avatars1.githubusercontent.com/u/37482325?s=460&u=21082310a42dc805cefd25c8ad863744aa94b996&v=4"
+                        alt="J-Eugenio"
+                    />
+                    <div>
+                        <strong>J-Eugênio</strong>
+                        <p>A Software / Technology Hobbyist / Enthusiast.</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img
+                        src="https://avatars1.githubusercontent.com/u/37482325?s=460&u=21082310a42dc805cefd25c8ad863744aa94b996&v=4"
+                        alt="J-Eugenio"
+                    />
+                    <div>
+                        <strong>J-Eugênio</strong>
+                        <p>A Software / Technology Hobbyist / Enthusiast.</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img
+                        src="https://avatars1.githubusercontent.com/u/37482325?s=460&u=21082310a42dc805cefd25c8ad863744aa94b996&v=4"
+                        alt="J-Eugenio"
+                    />
+                    <div>
+                        <strong>J-Eugênio</strong>
+                        <p>A Software / Technology Hobbyist / Enthusiast.</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    )
 }
 
 export default Dashboard

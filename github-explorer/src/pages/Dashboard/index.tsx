66baf9f6e4250ac889e23dec52b255f9784adcaa
José Eugênio dos Santos <37482325+J-Eugenio@users.noreply.github.com>
2020-09-05 14:55:50 -0300

    Organização de pastas e estilo global

---
+++ github-explorer/src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Titulo } from './styles'
+
+const Dashboard: React.FC = () => {
+    return <Titulo>Explore repositórios no Github</Titulo>
+}
+
+export default Dashboard

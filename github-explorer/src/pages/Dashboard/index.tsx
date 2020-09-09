c08e208ba0605807ed2105444b85d23b465d397f
José Eugênio dos Santos <37482325+J-Eugenio@users.noreply.github.com>
2020-09-09 14:47:10 -0300

    Salvando no Storage/ Navegando entre rotas

--- github-explorer/src/pages/Dashboard/index.tsx
+++ github-explorer/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState, FormEvent } from 'react'
+import React, { useState, useEffect, FormEvent } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 import api from '../../services/api'
 
 import logoImg from '../../assets/logo.svg'
@@ -18,7 +19,19 @@ const Dashboard: React.FC = () => {
 
     const [newRepo, setNewRepo] = useState('')
     const [inputError, setInputError] = useState('')
-    const [repositories, setRepositories] = useState<Repository[]>([])
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storageRepositories = localStorage.getItem('@GithubExplorer:repositories')
+
+        if (storageRepositories) {
+            return JSON.parse(storageRepositories)
+        } else {
+            return []
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories))
+    }, [repositories])
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault()
@@ -58,7 +71,7 @@ const Dashboard: React.FC = () => {
 
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="teste">
+                    <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
                         <img
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login}
@@ -69,7 +82,7 @@ const Dashboard: React.FC = () => {
                         </div>
 
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>

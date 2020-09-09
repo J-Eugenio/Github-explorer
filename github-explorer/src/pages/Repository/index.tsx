5bd6924d4274837aad6b9c1584dfeabf39aa0d19
José Eugênio dos Santos <37482325+J-Eugenio@users.noreply.github.com>
2020-09-09 15:18:16 -0300

    Estilizando Detalhes

--- github-explorer/src/pages/Repository/index.tsx
+++ github-explorer/src/pages/Repository/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
-import { useRouteMatch } from 'react-router-dom'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import { useRouteMatch, Link } from 'react-router-dom'
 
-interface RepositoryParams{
+import logoImg from '../../assets/logo.svg'
+import {
+    Header,
+    RepositoryInfo,
+    Issues
+} from './styles'
+
+interface RepositoryParams {
     repository: string
 }
 
@@ -9,7 +17,53 @@ const Repository: React.FC = () => {
 
     const { params } = useRouteMatch<RepositoryParams>()
 
-    return <h1>Repository : {params.repository}</h1>
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars1.githubusercontent.com/u/37482325?s=460&u=21082310a42dc805cefd25c8ad863744aa94b996&v=4" alt="Img" />
+                    <div>
+                        <strong>J-Eugenio</strong>
+                        <p>descrição do repositorio</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link      
+                    to={`teste`}>
+                    <div>
+                        <strong>aaaaaaa</strong>
+                        <p>nnnnnnnnnnnnnnnn</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    )
 }
 
 export default Repository
